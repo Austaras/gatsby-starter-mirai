8f75d7fc6f20192f2bd87db93927094a856f055e
austaras <austaras@outlook.com>
2020-03-09 16:10:01 +0800

    take back PT Sans

--- gatsby/ssr.tsx
+++ gatsby/ssr.tsx
@@ -18,7 +18,8 @@ export const onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }: Pr
   headers.push(
     <link
       rel='stylesheet'
-      href='https://fonts.googleapis.com/css?family=Merriweather+Sans:300,300i,400|Raleway:300&display=swap'
+      href={`https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400&
+            family=Raleway:wght@300&display=swap`}
     />
   )
   replaceHeadComponents(headers)
