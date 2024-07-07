5010dcc9c0fe522b816ba8b5e15c7a94b3c10cab
austaras <austaras@outlook.com>
2024-07-07 09:48:27 +0800

    remove prismjs

--- src/index/index.tsx
+++ src/index/index.tsx
@@ -1,6 +1,3 @@
-import 'prismjs/themes/prism-tomorrow.css'
-import 'prismjs/plugins/line-numbers/prism-line-numbers.css'
-
 import * as style from './index.module.scss'
 
 import { Pagination } from './pagination'
